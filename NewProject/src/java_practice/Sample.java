59bb4cc3a3e7fe8939268f2f6c023bc15bba6205
sambasiva <sambasiva@gmail.com>
2019-10-22 14:17:55 +0530

    commit

---
+++ NewProject/src/java_practice/Sample.java
@@ -0,0 +1,12 @@
+package java_practice;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Sambasiva");
+	}
+
+}
